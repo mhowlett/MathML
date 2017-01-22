8ba21d919a6e2cd9db0f4c4d8c0841ef01f56b06
Matt Howlett <matt@mhowlett.com>
2017-01-22 10:21:14 -0800

    initial commit

---
+++ src/MathML/Classifier.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace MathML
+{
+    public static class Classifier
+    {
+        public static double Sigmoid(double v)
+            => 1.0 / (1 + Math.Exp(-v));
+
+        public static double DotProduct(double[] xs, double[] ys)
+            => xs.Zip(ys, (x, y) => x * y).Sum();
+
+        public static double Hypothesis(double[] x, double[] theta)
+            => Sigmoid(DotProduct(x, theta));
+
+        public static double Probability(double[] x, double[] theta)
+            => Hypothesis(x, theta);
+
+        public static double Cost(double[] x, double y, double[] theta)
+            => -y * Math.Log(Hypothesis(x, theta)) - (1 - y) * Math.Log(1 - Hypothesis(x, theta));
+
+        public static double Cost(double[][] xs, double[] ys, double[] theta)
+            => xs.Zip(ys, (x, y) => Cost(x, y, theta)).Sum() / xs.Length;
+
+        public static double[] ScalarProduct(double[] xs, double y)
+            => xs.Select(x => x * y).ToArray();
+
+        public static double[] Gradient(double[] x, double y, double[] theta)
+            => ScalarProduct(x, (Hypothesis(x, theta) - y));
+
+        public static double[] VectorAdd(double[][] xs1)
+            => xs1.Aggregate(new double[xs1[0].Length], (accum, xs) => accum.Zip(xs, (a, x) => a + x).ToArray());
+
+        public static double[] Gradient(double[][] xs, double[] ys, double[] theta)
+            => VectorAdd(xs.Zip(ys, (x, y) => Gradient(x, y, theta)).ToArray());
+
+        public static double[] VectorAdd(double[] xs, double[] ys)
+            => xs.Zip(ys, (x, y) => x + y).ToArray();
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="xs"></param>
+        /// <param name="ys"></param>
+        /// <param name="theta"></param>
+        /// <param name="alpha"></param>
+        /// <param name="delta"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public static double[] Train(double[][] xs, double[] ys, double[] theta, double alpha, double delta, ref int limit)
+        {
+            double prevCost = Classifier.Cost(xs, ys, theta);
+            while (--limit > 0)
+            {
+                theta = VectorAdd(theta, ScalarProduct(Gradient(xs, ys, theta), -alpha));
+                var cost = Classifier.Cost(xs, ys, theta);
+                // Console.WriteLine($"cost: {cost}");
+                if (Math.Abs(cost - prevCost) < delta)
+                {
+                    break;
+                }
+                prevCost = cost;
+            }
+            return theta;
+        }
+    }
+}
---
+++ test/MathML.Test/Tests.cs
@@ -0,0 +1,94 @@
+﻿using System.Linq;
+using Xunit;
+
+
+namespace MathML.Test
+{
+    public class Tests
+    {
+        [Fact]
+        public void Sigmoid()
+        {
+            Assert.InRange(Classifier.Sigmoid(0), 0.49, 0.51);
+            Assert.InRange(Classifier.Sigmoid(2), 0.8, 1.0);
+            Assert.InRange(Classifier.Sigmoid(-2), 0, 0.2);
+        }
+
+        [Fact]
+        public void DotProduct()
+        {
+            Assert.Equal(Classifier.DotProduct(new double[] { 1, 2 }, new double[] { 2, 3 }), 8.0);
+        }
+
+        [Fact]
+        public void ScalarProduct()
+        {
+            Assert.Equal(Classifier.ScalarProduct(new double[] { 3, 4 }, 5), new double[] { 15, 20 });
+        }
+
+        [Fact]
+        public void VectorAdd()
+        {
+            Assert.Equal(
+                Classifier.VectorAdd(new double[] { 1, 1 }, new double[] { 3, 5 }), 
+                new double[] { 4, 6 }
+            );
+
+            var xs = new double[][]
+            {
+                new double[] { 1, 2 },
+                new double[] { 1, 3 },
+                new double[] { 1, 4 },
+                new double[] { 1, 5 }
+            };
+
+            Assert.Equal(Classifier.VectorAdd(xs), new double[] { 4, 14 });
+        }
+
+        [Fact]
+        public void Hypothesis()
+        {
+            var p = Classifier.Probability(
+                x: new double[] { 1, 1 }, 
+                theta: new double[] { -1, -1 }
+            );
+            Assert.InRange(p, 0, 0.5);
+
+            p = Classifier.Probability(
+                x: new double[] { 1, 1 }, 
+                theta: new double[] { 1, 1 }
+            );
+            Assert.InRange(p, 0.5, 1.0);
+        }
+
+        [Fact]
+        public void Train()
+        {
+            var xs = new double[][]
+            {
+                new double[] { 1, 2 },
+                new double[] { 1, 3 },
+                new double[] { 1, 4 },
+                new double[] { 1, 5 }
+            };
+            var ys = new double[] { 0, 0, 0, 1 };
+            var theta = new double[] { 1, 1 };
+
+            var limit = 100000;
+            theta = Classifier.Train(
+                xs, ys,
+                theta,
+                0.02,
+                0.0001,
+                ref limit
+            );
+
+            var ps = xs.Select(x => Classifier.Probability(x, theta)).ToList();
+
+            Assert.InRange(ps[0], 0, 0.1);
+            Assert.InRange(ps[1], 0, 0.1);
+            Assert.InRange(ps[2], 0, 0.1);
+            Assert.InRange(ps[3], 0.9, 1.0);
+        }
+    }
+}
