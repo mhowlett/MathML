9f48445a95996b55d886ee131281995feb0130e7
Matt Howlett <matt@mhowlett.com>
2017-02-05 13:22:25 -0800

    fast versions. alpha decay. class weight

--- src/MathML/Classifier.cs
+++ src/MathML/Classifier.cs
@@ -60,6 +60,104 @@ namespace MathML
                  1.0 / xs.Length
                );
 
+        unsafe public static void FastGradient(double[,] xs, double[] ys, double[] theta, double[] thetaGradientOut)
+        {
+            fixed (double* theta_fixed = theta)
+            fixed (double* ys_fixed = ys)
+            fixed (double* xs_fixed = xs)
+            fixed (double* result_fixed = thetaGradientOut)
+            {
+                double* ysPtr = ys_fixed;
+                double* xsPtr = xs_fixed;
+                double* resultPtr = result_fixed;
+                double* thetaPtr = theta_fixed;
+
+                for (int j=0; j<theta.Length; ++j)
+                {
+                    *(resultPtr++) = 0.0;
+                }
+
+                for (int i=0; i<ys.Length; ++i)
+                {
+                    double* xsPtr_save = xsPtr;
+                    double h = 0;
+                    thetaPtr = theta_fixed;
+                    for (int j=0; j<theta.Length; ++j)
+                    {
+                        h += *(thetaPtr++) * *(xsPtr++);
+                    }
+                    double delta = Sigmoid(h) - *(ysPtr++);
+
+                    resultPtr = result_fixed;
+                    for (int j=0; j<theta.Length; ++j)
+                    {
+                        *(resultPtr++) += delta * *(xsPtr_save++);
+                    }
+                }
+
+                resultPtr = result_fixed;
+                double trainingSetSize = ys.Length;
+                for (int j=0; j<theta.Length; ++j)
+                {
+                    *(resultPtr++) /= trainingSetSize;
+                }
+            }
+        }
+
+        unsafe public static void FastGradient(double[,] xs, double[] ys, double[] theta, double lambda, double weightClass0, double[] thetaGradientOut)
+        {
+            int thetaLen = theta.Length;
+
+            fixed (double* theta_fixed = theta)
+            fixed (double* ys_fixed = ys)
+            fixed (double* xs_fixed = xs)
+            fixed (double* thetaGradient_fixed = thetaGradientOut)
+            {
+                double* ysPtr = ys_fixed;
+                double* xsPtr = xs_fixed;
+                double* resultPtr = thetaGradient_fixed;
+                double* thetaPtr = theta_fixed;
+
+                for (int j=0; j<thetaLen; ++j)
+                {
+                    *(resultPtr++) = 0.0;
+                }
+                
+                double weightTotal = 0.0;
+                for (int i=0; i<ys.Length; ++i)
+                {
+                    double weight = ys[i] > 0.5 ? 1.0 : weightClass0;
+                    weightTotal += weight;
+                    
+                    double* xsPtr_save = xsPtr;
+                    double h = 0;
+                    thetaPtr = theta_fixed;
+                    for (int j=0; j<thetaLen; ++j)
+                    {
+                        h += *(thetaPtr++) * *(xsPtr++);
+                    }
+                    double delta = Sigmoid(h) - *(ysPtr++);
+
+                    resultPtr = thetaGradient_fixed;
+                    for (int j=0; j<thetaLen; ++j)
+                    {
+                        *(resultPtr++) += weight * delta * *(xsPtr_save++);
+                    }
+                }
+
+                double trainingSetSize = ys.Length;
+
+                resultPtr = thetaGradient_fixed;
+                *(resultPtr++) /= weightTotal;
+                thetaPtr = theta_fixed + 1;
+                for (int j=1; j<thetaLen; ++j)
+                {
+                    *resultPtr += lambda * *(thetaPtr++);
+                    *(resultPtr++) /= weightTotal;
+                }
+            }
+        }
+
         public static double[] Gradient(double[][] xs, double[] ys, double[] theta, double lambda)
             => ScalarProduct(
                  VectorAdd(
@@ -67,7 +165,7 @@ namespace MathML
                      ys, 
                      (fvs, y) => VectorAdd(
                                    Gradient(fvs, y, theta), 
-                                   ScalarProduct(new[] {0.0}.Concat(theta.Skip(1)).ToArray(), lambda)
+                                   ScalarProduct(new[] {0.0}.Concat(theta.Skip(1)).ToArray(), lambda/xs.Length)
                                  )
                    ).ToArray()
                  ), 
@@ -76,6 +174,23 @@ namespace MathML
 
         public static double[] VectorAdd(double[] xs, double[] ys)
             => xs.Zip(ys, (x, y) => x + y).ToArray();
+
+        unsafe public static void VectorAddFast(double[] xs, double[] ys, double[] resultOut)
+        {
+            fixed (double* xs_fixed = xs)
+            fixed (double* ys_fixed = ys)
+            fixed (double* result_fixed = resultOut)
+            {
+                double* xsPtr = xs_fixed;
+                double* ysPtr = ys_fixed;
+                double* resultPtr = result_fixed;
+
+                for (int i=0; i<xs.Length; ++i)
+                {
+                    *(resultPtr++) = *(xsPtr++) + *(ysPtr++);
+                }
+            }
+        }
 
         public static double[] Train(double[][] xs, double[] ys, double[] theta, double minAlpha, double maxAlpha, ref int iterations)
         {
@@ -101,11 +216,11 @@ namespace MathML
         public static double[] Train(double[][] xs, double[] ys, double[] theta, double lambda, double minAlpha, double maxAlpha, ref int iterations)
         {
             var alpha = maxAlpha;
-            double prevCost = Classifier.Cost(xs, ys, theta, lambda);
+            double prevCost = Cost(xs, ys, theta, lambda);
             while (iterations-- > 0)
             {
                 theta = VectorAdd(theta, ScalarProduct(Gradient(xs, ys, theta, lambda), -alpha));
-                var cost = Classifier.Cost(xs, ys, theta, lambda);
+                var cost = Cost(xs, ys, theta, lambda);
                 if (cost > prevCost)
                 {
                     alpha /= 2;
@@ -119,5 +234,43 @@ namespace MathML
             return theta;
         }
 
+        unsafe public static double[] FastTrain(
+            double[,] xs, 
+            double[] ys, 
+            double[] theta, 
+            double lambda, 
+            double weightClass0,
+            double alpha, 
+            int iterations,
+            int halfLives)
+        {
+            double[] thetaGradientOut = new double[theta.Length];
+            int thetaLen = theta.Length;
+
+            double alphaHalfLife = iterations / halfLives;
+            double alphaDecay = Math.Pow(0.5, 1.0/alphaHalfLife) - 1;
+
+            fixed (double* thetaGradient_fixed = thetaGradientOut)
+            fixed (double* theta_fixed = theta)
+            {
+                while (iterations-- > 0)
+                {
+                    FastGradient(xs, ys, theta, lambda, weightClass0, thetaGradientOut);
+
+                    double* thetaPtr = theta_fixed;
+                    double* thetaGradientPtr = thetaGradient_fixed;
+
+                    for (int i=0; i<thetaLen; ++i)
+                    {
+                        *thetaPtr = *thetaPtr - alpha * *(thetaGradientPtr++);
+                        thetaPtr += 1;
+                    }
+
+                    alpha *= (1+alphaDecay);
+                }
+            }
+
+            return theta;
+        }
     }
 }
--- test/MathML.Test/Tests.cs
+++ test/MathML.Test/Tests.cs
@@ -86,6 +86,104 @@ namespace MathML.Test
         }
 
         [Fact]
+        public void FastGradient()
+        {
+            var xs = new double[][]
+            {
+                new double[] { 1, 2 },
+                new double[] { 1, 3 },
+                new double[] { 1, 4 },
+                new double[] { 1, 5 }
+            };
+            var xs2 = new double[4, 2] 
+            {
+                { 1, 2 },
+                { 1, 3 },
+                { 1, 4 },
+                { 1, 5}
+            };
+
+            var ys = new double[] { 0, 0, 0, 1 };
+            var theta = new double[] { 1, 1 };
+            var thetaGradient = new double[2];
+
+            var grad1 = Classifier.Gradient(xs, ys, theta);
+            Classifier.FastGradient(xs2, ys, theta, thetaGradient);
+
+            Assert.InRange(grad1[0], thetaGradient[0]-0.001, thetaGradient[0]+0.001);
+            Assert.InRange(grad1[1], thetaGradient[1]-0.001, thetaGradient[1]+0.001);
+        }
+
+        [Fact]
+        public void FastGradientLambda()
+        {
+            var xs = new double[][]
+            {
+                new double[] { 1, 2, 3 },
+                new double[] { 1, 3, 4 },
+                new double[] { 1, 4, 5 },
+                new double[] { 1, 5, 6 }
+            };
+            var xs2 = new double[4, 3] 
+            {
+                { 1, 2, 3 },
+                { 1, 3, 4 },
+                { 1, 4, 5 },
+                { 1, 5, 6 }
+            };
+
+            var ys = new double[] { 0, 0, 0, 1 };
+            var theta = new double[] { 1, 1, 1 };
+            var thetaGradient = new double[3];
+
+            var grad1 = Classifier.Gradient(xs, ys, theta, 0.1);
+            Classifier.FastGradient(xs2, ys, theta, 0.1, 1.0, thetaGradient);
+
+            Assert.InRange(grad1[0], thetaGradient[0]-0.001, thetaGradient[0]+0.001);
+            Assert.InRange(grad1[1], thetaGradient[1]-0.001, thetaGradient[1]+0.001);
+            Assert.InRange(grad1[2], thetaGradient[2]-0.001, thetaGradient[2]+0.001);
+        }
+
+        [Fact]
+        public void FastTrain()
+        {
+            var xs1 = new double[][]
+            {
+                new double[] { 1, 2 },
+                new double[] { 1, 3 },
+                new double[] { 1, 4 },
+                new double[] { 1, 5 }
+            };
+
+            var xs2 = new double[4,2]
+            {
+                { 1, 2 },
+                { 1, 3 },
+                { 1, 4 },
+                { 1, 5 }
+            };
+            var ys = new double[] { 0, 0, 0, 1 };
+            var theta = new double[] { 1, 1 };
+
+            theta = Classifier.FastTrain(
+                xs2, ys,
+                theta,
+                lambda: 0.0,
+                weightClass0: 1.0,
+                alpha: 0.5,
+                iterations: 100000,
+                halfLives: 3
+            );
+
+            var ps = xs1.Select(x => Classifier.Probability(x, theta)).ToList();
+
+            Assert.InRange(ps[0], 0, 0.1);
+            Assert.InRange(ps[1], 0, 0.1);
+            Assert.InRange(ps[2], 0, 0.1);
+            Assert.InRange(ps[3], 0.9, 1.0);
+        }
+
+        [Fact]
         public void Train()
         {
             var xs = new double[][]
